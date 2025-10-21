14af81ce6d7660d7a2aab56610cee7498cfcb758
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-10-21 15:24:18 +0200

    Change default port

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,7 +9,7 @@ export const DEVELOPMENT = process.env.NODE_ENV === "development";
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME ?? "192.168.0.19";
 const SERVER_PORT = process.env.PORT
   ? Number(process.env.PORT)
-  : 3000;
+  : 8080;
 
 export const SERVER = {
   HOSTNAME: SERVER_HOSTNAME,
